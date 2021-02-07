4ece2244a76cf04855b93dc3f79ad29087c892bf
Simon Repp <simon@fdpl.io>
2021-02-07 16:54:24 +0100

    Introduce generic page layout rendering

--- src/render.rs
+++ src/render.rs
@@ -8,39 +8,52 @@ use crate::release::Release;
 const DOWNLOAD_INCLUDES_TEXT: &str = "Includes high-quality download in MP3, FLAC and more.";
 const PAYING_SUPPORTERS_TEXT: &str = "Paying supporters make a dignified life for artists possible, giving them some financial security in their life.";
 
+fn layout(page_depth: usize, body: &str, title: &str) -> String {
+    formatdoc!(
+        r#"
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>{title}</title>
+                    <meta charset="utf-8">
+                    <meta name="description" content="{title}">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <!-- TODO: <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="{root_prefix}feed.rss"> -->
+                    <link href="{root_prefix}styles.css" rel="stylesheet">
+                </head>
+                <body>
+                    {body}
+                </body>
+            </html>
+        "#,
+        body=body,
+        root_prefix=("../".repeat(page_depth)),
+        title=title
+    )
+}
+
 pub fn render_download(artist: &Artist, release: &Release) -> String {
     let release_cover_rendered = match &release.cover {
         Some(image) => format!(r#"<img class="cover" src="../../{}">"#, image.transcoded_file),
         None => String::from(r#"<div class="cover"></div>"#)
     };
     
-    formatdoc!(
+    let body = formatdoc!(
         r#"
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>Download {release_title}</title>
-                    <meta charset="utf-8">
-                    <meta name="description" content="Download {release_title}">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="feed.rss"> <!--TODO-->
-                    <link href="../../styles.css" rel="stylesheet">
-                </head>
-                <body>
-                    {release_cover_rendered}
-                    
-                    <h1>Download {release_title}</h1>
-                    <div>by <a href="{artist_href}">{artist_name}</a></div>
-                    
-                    <div><a download href="original.zip">Zip Archive</a></div>
-                </body>
-            </html>
+            {release_cover_rendered}
+            
+            <h1>Download {release_title}</h1>
+            <div>by <a href="{artist_href}">{artist_name}</a></div>
+            
+            <div><a download href="original.zip">Zip Archive</a></div>
         "#,
         artist_href="TODO",
         artist_name=artist.name,
         release_cover_rendered=release_cover_rendered,
         release_title=release.title
-    )
+    );
+    
+    layout(2, &body, &release.title)
 }
 
 pub fn render_release(artist: &Artist, release: &Release) -> String {
@@ -102,31 +115,18 @@ pub fn render_release(artist: &Artist, release: &Release) -> String {
         .collect::<Vec<String>>()
         .join("<br><br>\n");
 
-    formatdoc!(
+    let body = formatdoc!(
         r#"
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>{release_title}</title>
-                    <meta charset="utf-8">
-                    <meta name="description" content="{release_title}">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="feed.rss"> <!--TODO-->
-                    <link href="../styles.css" rel="stylesheet">
-                </head>
-                <body>
-                    <h1>{release_title}</h1>
-                    <div>by <a href="{artist_href}">{artist_name}</a></div>
-                    
-                    {download_option_rendered}
-                    
-                    {release_cover_rendered}
+            <h1>{release_title}</h1>
+            <div>by <a href="{artist_href}">{artist_name}</a></div>
+            
+            {download_option_rendered}
+            
+            {release_cover_rendered}
 
-                    <div>{tracks}</div>
-                    
-                    <div>{release_text}</div>
-                </body>
-            </html>
+            <div>{tracks}</div>
+            
+            <div>{release_text}</div>
         "#,
         artist_href="TODO",
         artist_name=artist.name,
@@ -135,7 +135,9 @@ pub fn render_release(artist: &Artist, release: &Release) -> String {
         release_text=release.text.as_ref().unwrap_or(&String::new()),
         release_title=release.title,
         tracks=tracks_rendered
-    )
+    );
+    
+    layout(1, &body, &release.title)
 }
 
 pub fn render_releases(artist: &Artist, catalog: &Catalog) -> String {
@@ -148,7 +150,7 @@ pub fn render_releases(artist: &Artist, catalog: &Catalog) -> String {
             };
             
             format!(
-                r#"{release_cover_rendered} <a href="{release_slug}">{release_title}</a>"#,
+                r#"{release_cover_rendered} <a href="{release_slug}/">{release_title}</a>"#,
                 release_cover_rendered=release_cover_rendered,
                 release_slug=release.slug,
                 release_title=release.title
@@ -157,26 +159,14 @@ pub fn render_releases(artist: &Artist, catalog: &Catalog) -> String {
         .collect::<Vec<String>>()
         .join("<br><br>\n");
     
-    formatdoc!(
+    let body = formatdoc!(
         r#"
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>{artist_name}</title>
-                    <meta charset="utf-8">
-                    <meta name="description" content="{artist_name}">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="feed.rss"> <!--TODO-->
-                    <link href="styles.css" rel="stylesheet">
-                </head>
-                <body>
-                    <h1>{artist_name}</h1>
-
-                    <div>{releases}</div>
-                </body>
-            </html>
+            <h1>{artist_name}</h1>
+            <div>{releases}</div>
         "#,
         artist_name=artist.name,
         releases=releases_rendered
-    )
+    );
+    
+    layout(0, &body, &artist.name)
 }
