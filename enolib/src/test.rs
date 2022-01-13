b8537b73b0a507485938f92ee43b95c3612dd0cc
Simon Repp <simon@fdpl.io>
2022-01-13 19:38:33 +0100

    Extend tests for enolib

--- enolib/src/test.rs
+++ enolib/src/test.rs
@@ -3,19 +3,69 @@ use indoc::formatdoc;
 use crate::{FieldContent, Kind, parse};
 
 #[test]
+fn test_attributes() {
+    let result = parse(&formatdoc!(r#"
+        > comment
+        field:
+        attribute = value
+    "#));
+
+    let document = result.unwrap();
+    let element = document.elements.first().unwrap();
+
+    assert_eq!(element.comment.as_deref(), Some("comment"));
+    assert_eq!(element.key, "field");
+    assert_eq!(element.line_number, 2);
+
+    match &element.kind {
+        Kind::Field(FieldContent::Attributes(attributes)) => {
+            assert_eq!(attributes.len(), 1);
+
+            let attribute = attributes.first().unwrap();
+
+            assert_eq!(attribute.key, "attribute");
+            assert_eq!(attribute.line_number, 3);
+            assert_eq!(attribute.value, "value");
+        }
+        _ => panic!("Field with attributes expected")
+    }
+}
+
+#[test]
+fn test_continuations() {
+    let result = parse(&formatdoc!(r#"
+        field:
+        | a
+        \ b
+        | c
+    "#));
+
+    let document = result.unwrap();
+    let element = document.elements.first().unwrap();
+
+    match &element.kind {
+        Kind::Field(FieldContent::Value(value)) => {
+            assert_eq!(value, "a bc");
+        }
+        _ => panic!("Field with value expected")
+    }
+}
+
+#[test]
 fn test_document_comment() {
     let result = parse(&formatdoc!(r#"
         >  alice
         > bob
         > carter
-        
+
         empty
     "#));
-    
-    assert_eq!(
-        &result.unwrap().comment.unwrap(),
-        " alice\nbob\ncarter"
-    );
+
+    let document = result.unwrap();
+    let element = document.elements.first().unwrap();
+
+    assert_eq!(element.line_number, 5);
+    assert_eq!(document.comment.as_deref(), Some(" alice\nbob\ncarter"));
 }
 
 #[test]
@@ -25,12 +75,13 @@ fn test_embed() {
         value
         -- embed
     "#));
-    
+
     let document = result.unwrap();
     let element = document.elements.first().unwrap();
-    
+
     assert_eq!(element.key, "embed");
-    
+    assert_eq!(element.line_number, 1);
+
     match &element.kind {
         Kind::Embed(Some(value)) => {
             assert_eq!(value, "value");
@@ -40,13 +91,70 @@ fn test_embed() {
 }
 
 #[test]
-fn test_escaped_key() {
+fn test_empty() {
+    let result = parse("empty");
+
+    let document = result.unwrap();
+    let element = document.elements.first().unwrap();
+
+    assert_eq!(element.key, "empty");
+    assert_eq!(element.line_number, 1);
+
+    match element.kind {
+        Kind::Empty => (),
+        _ => panic!("Empty expected")
+    }
+}
+
+#[test]
+fn test_empty_with_escaped_key() {
     let result = parse("`` `empty` ``");
-    
-    assert_eq!(
-        &result.unwrap().elements.first().unwrap().key,
-        "`empty`"
-    );
+
+    let document = result.unwrap();
+    let element = document.elements.first().unwrap();
+
+    assert_eq!(element.key, "`empty`");
+
+    match element.kind {
+        Kind::Empty => (),
+        _ => panic!("Empty expected")
+    }
+}
+
+#[test]
+fn test_items() {
+    let result = parse(&formatdoc!(r#"
+        field:
+        > comment1
+        - item1
+        > comment2
+        - item2
+    "#));
+
+    let document = result.unwrap();
+    let element = document.elements.first().unwrap();
+
+    assert_eq!(element.key, "field");
+    assert_eq!(element.line_number, 1);
+
+    match &element.kind {
+        Kind::Field(FieldContent::Items(items)) => {
+            assert_eq!(items.len(), 2);
+
+            let mut iter = items.iter();
+
+            let first = iter.next().expect("First item expected");
+            assert_eq!(first.comment.as_deref(), Some("comment1"));
+            assert_eq!(first.line_number, 3);
+            assert_eq!(first.value,"item1");
+
+            let second = iter.next().expect("Second item expected");
+            assert_eq!(second.comment.as_deref(), Some("comment2"));
+            assert_eq!(second.line_number, 5);
+            assert_eq!(second.value,"item2");
+        }
+        _ => panic!("Field with items expected")
+    }
 }
 
 #[test]
@@ -56,21 +164,22 @@ fn test_section() {
         # section
         field: value
     "#));
-    
+
     let document = result.unwrap();
     let element = document.elements.first().unwrap();
 
     assert_eq!(element.comment.as_deref(), Some("associated comment"));
-    assert_eq!(&element.key, "section");
-    
+    assert_eq!(element.key, "section");
+
     match &element.kind {
         Kind::Section(elements) => {
             assert_eq!(elements.len(), 1);
-            
+
             let element = elements.first().unwrap();
-            
-            assert_eq!(&element.key, "field");
-            
+
+            assert_eq!(element.key, "field");
+            assert_eq!(element.line_number, 3);
+
             match &element.kind {
                 Kind::Field(FieldContent::Value(value)) => {
                     assert_eq!(value, "value");
@@ -85,7 +194,6 @@ fn test_section() {
 #[test]
 fn test_parse() {
     let result = parse("field: value");
-    
+
     assert!(result.is_ok());
 }
-
