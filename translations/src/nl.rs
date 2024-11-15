1101b941b1371e9e60724378d54fb46c7915711d
n3wjack <n3wjack@n3wjack.net>
2024-11-15 22:00:34 +0000

    Update NL translations.

--- translations/src/nl.rs
+++ translations/src/nl.rs
@@ -5,75 +5,80 @@
 use super::{Translations, Unreviewed};
 
 pub const NL: Translations = Translations {
-    audio_format_alac: Unreviewed("Verliesloos en gecomprimeerd, als je uitsluitend Apple-producten gebruikt, kies dan voor FLAC"),
-    audio_format_average: Unreviewed("Gemiddelde compressie, geschikt als uw speler geen betere formaten ondersteunt"),
-    audio_format_flac: Unreviewed("Verliesloos en gecomprimeerd, beste keuze voor archivering"),
-    audio_format_mp3: Unreviewed("Inefficiënte compressie, geschikt als compatibiliteit met oudere spelers nodig is"),
-    audio_format_opus_48: Unreviewed("Uitstekende compressie, zuinige kwaliteit, goede keuze als de ruimte beperkt is"),
-    audio_format_opus_96: Unreviewed("Uitstekende compressie, standaard kwaliteit, goede keuze voor offline luisteren"),
-    audio_format_opus_128: Unreviewed("Uitstekende compressie, hoogste kwaliteit, beste keuze voor offline luisteren"),
-    audio_format_uncompressed: Unreviewed("Ongecomprimeerde grote bestanden, alleen geschikt voor audioproductie"),
-    audio_player_widget_for_xxx: Unreviewed(r#"Audiospelerwidget voor "{title}""#),
-    auto_generated_cover: Unreviewed("Automatisch gegenereerde omslagafbeelding"),
-    available_formats: Unreviewed("Beschikbare formaten:"),
-    close: Unreviewed("Sluiten"),
-    copied: Unreviewed("Gekopieerd"),
-    copy: Unreviewed("Kopiëren"),
-    confirm: Unreviewed("Bevestigen"),
-    r#continue: Unreviewed("Voortzetten"),
-    cover_image: Unreviewed("Omslagafbeelding"),
-    default_unlock_text: Unreviewed("Jij moet een code invullen om deze downloads te ontsluiten. Vraag de websitebeheerder hoe je er een kan verkrijgen."),
-    downloads: Unreviewed("Downloads"),
-    downloads_permalink: Unreviewed("downloads"),
-    embed: Unreviewed("Inzetten"),
-    embed_entire_release: Unreviewed("De heele release inzetten"),
-    enter_code_here: Unreviewed("Vul hier het code in"),
-    extras: Unreviewed("Extra's"),
-    failed: Unreviewed("Mislukt"),
-    feed: Unreviewed("Feed"),
-    fixed_price: Unreviewed("Vaste prijs:"),
-    image_descriptions: Unreviewed("Afbeeldingsomschrijving"),
-    image_descriptions_guide: Unreviewed("\
-Miljoenen mensen surfen op internet met behulp van schermlezers \
-omdat ze niet (of niet goed genoeg) kunnen zien. Afbeeldingen \
-zonder tekstuele beschrijvingen zijn voor hen ontoegankelijk, \
-en daarom moeten we de moeite nemen om er beeldbeschrijvingen \
-voor te bieden.<br><br>\
-\
-Raadpleeg de Faircamp README voor het toevoegen van \
-afbeeldingsbeschrijvingen, het is eenvoudig en een \
-vriendelijke daad.<br><br>\
-\
-Hier zijn een paar tips voor het schrijven van goede afbeeldingsomschrijvingen:<br>\
-- Elke beschrijving is beter dan geen beschrijving, maak je geen zorgen dat je het verkeerd doet.<br>\
-- Maak het beknopt. Schrijf zoveel als nodig is, maar houd het tegelijkertijd zo kort mogelijk.<br>\
-- Niet interpreteren. Beschrijf wat er is en relevant voor het begrip ervan, analyseer niet verder dan dat.<br>\
-- Je kunt kleuren gebruiken waar dat zinvol is; veel mensen zijn pas later hun gezichtsvermogen kwijtgeraakt en begrijpen en waarderen kleuren."),
-    image_descriptions_permalink: Unreviewed("afbeeldingsomschrijvingen"),
-    made_or_arranged_payment: Unreviewed("Ik heb de betaling gedaan of geregeld"),
-    missing_image_description_note: Unreviewed("Ontbrekende afbeeldingsomschrijving<br>Klik om meer te leren"),
-    more: Unreviewed("Meer"),
-    name_your_price: Unreviewed("Noem je prijs"),
-    purchase_downloads: Unreviewed("Downloads kopen"),
-    purchase_permalink: Unreviewed("kopen"),
-    recommended_format: Unreviewed("Aanbevolen Formaat"),
-    rss_feed: Unreviewed("RSS Feed"),
-    unlock: Unreviewed("Ontsluiten"),
-    unlock_downloads: Unreviewed("Downloads ontsluiten"),
-    unlock_permalink: Unreviewed("ontsluiten"),
-    unlock_code_seems_incorrect: Unreviewed("De ontgrendelingscode lijkt onjuist te zijn. Controleer op typefouten."),
-    unlock_manual_instructions: Unreviewed("\
-Om de download te ontgrendelen, breng je de hieronder beschreven \
-wijzigingen aan in het adres in de adresbalk van jouw browser.\
-<br><br>\
-Voordat je dat doet houd er rekening mee dat verkeerde codes \
-of adreswijzigingen u naar een 404-pagina leiden. In het geval \
-gebruik de terugknop en volg de voorschriften nauwkeurig opnieuw.\
-<br><br>\
-Vervang het laatste deel van het adres - /{unlock_permalink}/{page_hash}{index_suffix} - \
-met /{downloads_permalink}/[your-unlock-code]{index_suffix} en druk vervolgens op Enter."),
-    up_to_xxx: Unreviewed("Tot {xxx}"),
-    xxx_and_others: Unreviewed(r#"{xxx} en <a href="{others_link}">meer</a>"#),
-    xxx_or_more: Unreviewed("{xxx} of meer"),
-    ..Translations::UNTRANSLATED
+    audio_format_alac: Reviewed("Verliesloos en gecomprimeerd, als je uitsluitend Apple-producten gebruikt, kies dan dit i.p.v. FLAC"),
+    audio_format_average: Reviewed("Gemiddelde compressie, geschikt als uw speler geen betere formaten ondersteunt"),
+    audio_format_flac: Reviewed("Verliesloos en gecomprimeerd, beste keuze voor archivering"),
+    audio_format_mp3: Reviewed("Inefficiënte compressie, geschikt als compatibiliteit met oudere spelers nodig is"),
+    audio_format_opus_128: Reviewed("Uitstekende compressie, hoogste kwaliteit, beste keuze voor offline luisteren"),
+    audio_format_opus_48: Reviewed("Uitstekende compressie, zuinige kwaliteit, goede keuze als de ruimte beperkt is"),
+    audio_format_opus_96: Reviewed("Uitstekende compressie, standaard kwaliteit, goede keuze voor offline luisteren"),
+    audio_format_uncompressed: Reviewed("Ongecomprimeerde grote bestanden, alleen geschikt voor audioproductie"),
+    audio_player_widget_for_xxx: Reviewed(r#"Audiospelerwidget voor "{title}""#),
+    auto_generated_cover: Reviewed("Automatisch gegenereerde hoesafbeelding"),
+    available_formats: Reviewed("Beschikbare formaten:"),
+    browse: Reviewed("Bladeren"),
+    buy: Reviewed("Koop"),
+    close: Reviewed("Sluiten"),
+    confirm: Reviewed("Bevestigen"),
+    continue: Reviewed("Doorgaan"),
+    copied: Reviewed("Gekopieerd"),
+    copy: Reviewed("Kopiëren"),
+    copy_link: Reviewed("Kopieer link"),
+    cover_image: Reviewed("Hoesafbeelding"),
+    default_unlock_text: Reviewed("Jij moet een code invullen om deze downloads te ontsluiten. Vraag de websitebeheerder hoe je er een kan verkrijgen."),
+    dimmed: Reviewed("Gedimd"),
+    download: Reviewed("Downloaden"),
+    downloads: Reviewed("Downloads"),
+    downloads_permalink: Reviewed("downloads"),
+    embed: Reviewed("Invoegen"),
+    embed_entire_release: Reviewed("De hele release invoegen"),
+    enter_code_here: Reviewed("Vul hier de code in"),
+    external_link: Reviewed("Externe link"),
+    extras: Reviewed("Extra's"),
+    failed: Reviewed("Mislukt"),
+    feed: Reviewed("Feed"),
+    fixed_price: Reviewed("Vaste prijs:"),
+    image_descriptions: Reviewed("Afbeeldingsomschrijvingen"),
+    image_descriptions_guide: Reviewed("Miljoenen mensen surfen op internet met behulp van schermlezers omdat ze niet (of niet goed genoeg) kunnen zien. Afbeeldingen zonder tekstuele beschrijvingen zijn voor hen ontoegankelijk, en daarom moeten we de moeite nemen om afbeeldingsomschrijvingen aan te bieden.<br><br>Raadpleeg de Faircamp README voor het toevoegen van afbeeldingsomschrijvingen, het is eenvoudig en een vriendelijke daad.<br><br>Hier zijn een paar tips voor het schrijven van goede afbeeldingsomschrijvingen:<br>- Elke beschrijving is beter dan geen beschrijving, maak je geen zorgen dat je het verkeerd doet.<br>- Maak het beknopt. Schrijf zoveel als nodig is, maar houd het tegelijkertijd zo kort mogelijk.<br>- Niet interpreteren. Beschrijf wat er is en relevant voor het begrip ervan, analyseer niet verder dan dat.<br>- Je kunt kleuren gebruiken waar dat zinvol is; veel mensen zijn pas later hun gezichtsvermogen kwijtgeraakt en begrijpen en waarderen kleuren."),
+    image_descriptions_permalink: Reviewed("afbeeldingsomschrijvingen"),
+    listen: Reviewed("Luister"),
+    loading: Reviewed("Aan het laden"),
+    m3u_playlist: Reviewed("M3U Afspeellijst"),
+    made_or_arranged_payment: Reviewed("Ik heb de betaling gedaan of geregeld"),
+    missing_image_description_note: Reviewed("Ontbrekende afbeeldingsomschrijving<br>Klik om meer te leren"),
+    more: Reviewed("Meer"),
+    mute: Reviewed("Dempen"),
+    muted: Reviewed("Gedempt"),
+    name_your_price: Reviewed("Noem je prijs"),
+    next_track: Reviewed("Volgende nummer"),
+    nothing_found_for_xxx: Reviewed("Niks gevonden voor '{query}'"),
+    pause: Reviewed("Pauze"),
+    play: Reviewed("Afspelen"),
+    playback_position: Reviewed("Afspeelpositie"),
+    player_closed: Reviewed("Speler gesloten"),
+    player_open_playing_xxx: Reviewed("Speler open, {title} aan het afspelen"),
+    previous_track: Reviewed("Vorig nummer"),
+    purchase_downloads: Reviewed("Downloads kopen"),
+    purchase_permalink: Reviewed("kopen"),
+    recommended_format: Reviewed("Aanbevolen Formaat"),
+    rss_feed: Reviewed("RSS Feed"),
+    search: Reviewed("Zoeken"),
+    showing_featured_items: Reviewed("Uitgelichte items tonen"),
+    showing_xxx_results_for_xxx: Reviewed("Weergave {count} resultaten voor '{query}'"),
+    this_site_was_created_with_faircamp: Reviewed("Deze site werd gemaakt met {faircamp_link}"),
+    unlisted: Reviewed("Niet vermeld"),
+    unlock: Reviewed("Ontsluiten"),
+    unlock_code_seems_incorrect: Reviewed("De ontgrendelingscode lijkt onjuist te zijn. Controleer op typefouten."),
+    unlock_downloads: Reviewed("Downloads ontsluiten"),
+    unlock_manual_instructions: Reviewed("Om de download te ontgrendelen, breng je de hieronder beschreven wijzigingen aan in het adres in de adresbalk van je browser.<br><br>Voordat je dat doet, hou er rekening mee dat verkeerde codes of adreswijzigingen u naar een 404-pagina leiden. In dit geval gebruik je de terugknop en volg de voorschriften nauwkeurig opnieuw.<br><br>Vervang het laatste deel van het adres - /{unlock_permalink}/{page_hash}{index_suffix} - met /{downloads_permalink}/[your-unlock-code]{index_suffix} en druk vervolgens op Enter."),
+    unlock_permalink: Reviewed("ontsluiten"),
+    unmute: Reviewed("Dempen opheffen"),
+    up_to_xxx: Reviewed("Tot {xxx}"),
+    visual_impairment: Reviewed("Visuele beperking"),
+    volume: Reviewed("Volume"),
+    xxx_and_others: Reviewed(r#"l {xxx} en <a href="{others_link}">meer</a>"#),
+    xxx_hours: Reviewed("{xxx} uren"),
+    xxx_minutes: Reviewed("{xxx} minuten"),
+    xxx_or_more: Reviewed("{xxx} of meer"),
+    xxx_seconds: Reviewed("{xxx} seconden")
 };
