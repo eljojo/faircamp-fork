15115367f65360cd9ececa7aecf0f978b3d6941d
Simon Repp <simon@fdpl.io>
2024-11-15 20:39:12 +0100

    Update french translations

--- translations/src/fr.rs
+++ translations/src/fr.rs
@@ -1,10 +1,10 @@
-// SPDX-FileCopyrightText: 2023-2024 Simon Repp
+// SPDX-FileCopyrightText: 2024 Vincent Knobil
 // SPDX-FileCopyrightText: 2024 sknob
 // SPDX-FileCopyrightText: 2023 Florian Antoine
 // SPDX-FileCopyrightText: 2023 Deborah Pickett
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use super::{Reviewed, Translations, Unreviewed};
+use super::{Reviewed, Translations};
 
 pub const FR: Translations = Translations {
     audio_format_alac: Reviewed("Sans perte et compacté. Préférer à FLAC si vous n’utilisez que des produits Apple."),
@@ -15,68 +15,72 @@ pub const FR: Translations = Translations {
     audio_format_opus_96: Reviewed("Excellente compression, qualité standard. Bon choix pour l’écoute hors ligne."),
     audio_format_opus_128: Reviewed("Excellente compression, qualité élevée. Meilleur choix pour l’écoute hors ligne."),
     audio_format_uncompressed: Reviewed("Fichiers volumineux non compactés. Réservé à la création audio."),
-    audio_player_widget_for_xxx: Unreviewed(r#"Widget de lecteur audio pour "{title}""#),
-    auto_generated_cover: Reviewed("Image de couverture générée automatiquement"),
+    audio_player_widget_for_xxx: Reviewed(r#"Widget de lecteur audio pour "{title}""#),
+    auto_generated_cover: Reviewed("Pochette d’album générée automatiquement"),
     available_formats: Reviewed("Formats disponibles:"),
+    browse: Reviewed("Parcourir"),
+    buy: Reviewed("Acheter"),
     close: Reviewed("Fermer"),
     copied: Reviewed("Copié"),
     copy: Reviewed("Copier"),
+    copy_link: Reviewed("Copier le lien"),
     confirm: Reviewed("Confirmer"),
     r#continue: Reviewed("Continuer"),
     cover_image: Reviewed("Image de couverture"),
     default_unlock_text: Reviewed("Vous devez entrer un code pour déverrouiller ces téléchargements. Demandez au gestionnaire du site comment en obtenir un."),
+    dimmed: Reviewed("Atténué"),
+    download: Reviewed("Télécharger"),
     downloads: Reviewed("Téléchargements"),
     downloads_permalink: Reviewed("telechargements"),
     embed: Reviewed("Intégrer"),
     embed_entire_release: Reviewed("Intégrer tout l'album"),
     enter_code_here: Reviewed("Entrer le code ici"),
+    external_link: Reviewed("Lien externe"),
     extras: Reviewed("Suppléments"),
     failed: Reviewed("Échec"),
     feed: Reviewed("Flux RSS"),
     fixed_price: Reviewed("Prix fixe:"),
     image_descriptions: Reviewed("Descriptions des images"),
-    image_descriptions_guide: Reviewed("\
-De millions des personnes naviguent sur le Web à l'aide de lecteurs \
-d'écran parce qu'elles ne voient pas (ou pas assez bien). \
-Les images sans descriptions textuelles leur sont inaccessibles, \
-et c'est pourquoi nous devrions faire l'effort de leur fournir \
-des desciptions d'images. \
-<br><br>\
-Consultez le faircamp README pour savoir comment ajouter \
-des descriptions d'images, c'est simple et c'est un acte de \
-gentillesse.\
-<br><br>\
-Voici quelques conseils pour rédiger de bonnes descriptions d'images:<br>\
-- N'importe quelle description vaut mieux que de ne pas avoir de description, ne vous inquiétez pas si vous vous trompez.<br>\
-- Soyez concis. Écrivez autant que nécessaire, mais en même temps soyez aussi bref que possible.<br>\
-- N'interprétez pas. Décrivez ce qui est là et pertinent pour la compréhension, n'analysez pas au-delà.<br>\
-- Vous pouvez utiliser des couleurs là où cela a du sens - beaucoup de gens n'ont perdu la vue que plus tard et comprennent et apprécient les coleurs."),
+    image_descriptions_guide: Reviewed("Des millions des personnes naviguent sur le Web à l'aide de lecteurs d'écran parce qu'elles ne voient pas (ou pas assez bien). Les images sans descriptions textuelles leur sont inaccessibles, et c'est pourquoi nous devons faire l'effort de leur fournir des descriptions d'images. <br><br>Consultez le fichier README de Faircamp pour savoir comment ajouter des descriptions d'images. C'est facile et c'est faire preuve de bienveillance.<br><br>Voici quelques conseils pour rédiger de bonnes descriptions d'images :<br>- Une description quelle qu’elle soit vaut mieux que l’absence de description, et n’ayez pas peur de vous tromper.<br>- Soyez concis. Rédigez autant que nécessaire, tout en étant le plus bref que possible.<br>- N'interprétez pas. Décrivez ce qui est présent et pertinent pour la compréhension, n'analysez pas au-delà.<br>- Vous pouvez mentionner les couleurs lorsque ça a du sens, car beaucoup de gens qui ont perdu la vue comprennent et apprécient les couleurs."),
     image_descriptions_permalink: Reviewed("descriptions-des-images"),
-    made_or_arranged_payment: Reviewed("J'ai effectué ou organisé le paiement"),
+    listen: Reviewed("Écouter"),
+    loading: Reviewed("Chargement"),
+    m3u_playlist: Reviewed("Playlist M3U"),
+    made_or_arranged_payment: Reviewed("J’ai effectué ou programmé le paiement"),
     missing_image_description_note: Reviewed("Description de l'image manquante<br>Cliquez pour en savoir plus"),
     more: Reviewed("Plus"),
-    name_your_price: Reviewed("Choisis ton prix"),
+    mute: Reviewed("Couper"),
+    muted: Reviewed("Coupé"),
+    name_your_price: Reviewed("Prix libre"),
+    next_track: Reviewed("Piste suivante"),
+    nothing_found_for_xxx: Reviewed("'{query}' introuvable"),
+    pause: Reviewed("Pause"),
+    play: Reviewed("Lecture"),
+    playback_position: Reviewed("Position de lecture"),
+    player_closed: Reviewed("Lecteur fermé"),
+    player_open_playing_xxx: Reviewed("Lecteur ouvert, {title} en écoute."),
+    previous_track: Reviewed("Piste précédente"),
     purchase_downloads: Reviewed("Acheter des téléchargements"),
     purchase_permalink: Reviewed("acheter"),
     recommended_format: Reviewed("Format recommandé"),
     rss_feed: Reviewed("Flux RSS"),
-    search: Reviewed("Chercher"),
+    search: Reviewed("Rechercher"),
+    showing_featured_items: Reviewed("Affichage des éléments en vedette"),
+    showing_xxx_results_for_xxx: Reviewed("Affichage de {count} résultats pour '{query}'"),
+    this_site_was_created_with_faircamp: Reviewed("Ce site a été créé avec {faircamp_link}"),
+    unlisted: Reviewed("Non répertorié"),
     unlock: Reviewed("Déverouiller"),
     unlock_downloads: Reviewed("Déverouiller les téléchargements"),
     unlock_permalink: Reviewed("deverouiller"),
     unlock_code_seems_incorrect: Reviewed("Le code de déverrouillage semble être incorrect, veuillez vérifier les fautes de frappe."),
-    unlock_manual_instructions: Reviewed("\
-Pour déverrouiller le téléchargement, veuillez effectuer les modifications \
-décrites ci-dessous dans la barre d'adresse de votre navigateur. \
-<br><br>\
-Avant de commencer, sachez que des codes ou des modifications d'adresse erronés \
-vous amènent à une page 404. Dans ce cas, utilisez le bouton Retour et suivez à \
-nouveau attentivement les instructions. \
-<br><br>\
-Remplacez la dernière partie de l'adresse - /{unlock_permalink}/{page_hash}{index_suffix} - \
-avec /{downloads_permalink}/[votre-code-de-deverrouillage]{index_suffix} et appuyez sur Entrée."),
+    unlock_manual_instructions: Reviewed("Pour déverrouiller le téléchargement, veuillez effectuer les modifications décrites ci-dessous dans la barre d'adresse de votre navigateur. <br><br>Avant de commencer, sachez que des codes ou les erreurs d’adresse vous renvoient à une page 404. Dans ce cas, utilisez le bouton Retour et suivez à nouveau attentivement les instructions. <br><br>Remplacez la dernière partie de l'adresse - /{unlock_permalink}/{page_hash}{index_suffix} - avec /{downloads_permalink}/[votre-code-de-deverrouillage]{index_suffix} et appuyez sur Entrée."),
+    unmute: Reviewed("Rétablir"),
     up_to_xxx: Reviewed("Jusqu'à {xxx}"),
+    visual_impairment: Reviewed("Déficience visuelle"),
+    volume: Reviewed("Volume"),
     xxx_and_others: Reviewed(r#"{xxx} et <a href="{others_link}">plus</a>"#),
+    xxx_hours: Reviewed("{xxx} heures"),
+    xxx_minutes: Reviewed("{xxx} minutes"),
     xxx_or_more: Reviewed("{xxx} ou plus"),
-    ..Translations::UNTRANSLATED
+    xxx_seconds: Reviewed("{xxx} secondes")
 };
