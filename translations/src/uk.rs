44aaacd90a93a47f1cadde833483eef2f24bcd18
Denys Nykula <nykula@ukr.net>
2025-10-09 12:56:45 +0300

    Update Ukrainian translation for 2025

--- translations/src/uk.rs
+++ translations/src/uk.rs
@@ -1,7 +1,7 @@
-// SPDX-FileCopyrightText: 2024 Denys Nykula
+// SPDX-FileCopyrightText: 2024-2025 Denys Nykula
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use super::{Reviewed, Translations, Unreviewed};
+use super::{Reviewed, Translations};
 
 pub const UK: Translations = Translations {
     audio_format_alac: Reviewed("Стиснення без втрат. Якщо користуєтесь лише пристроями Apple, оберіть це замість FLAC"),
@@ -12,10 +12,12 @@ pub const UK: Translations = Translations {
     audio_format_opus_96: Reviewed("Чудове стиснення, звичайна якість. Хороший вибір для офлайн-прослуховування"),
     audio_format_opus_128: Reviewed("Чудове стиснення, найвища якість. Найкращий вибір для офлайн-прослуховування"),
     audio_format_uncompressed: Reviewed("Нестиснені важкі файли. Годиться лише для продюсування музики"),
-    audio_player_widget_for_xxx: Unreviewed("Звуковий програвач «{title}»"),
+    audio_player_widget_for_xxx: Reviewed("Звуковий програвач «{title}»"),
     auto_generated_cover: Reviewed("Автоматично згенерована обкладинка"),
     available_formats: Reviewed("Доступні формати:"),
+    browse: Reviewed("Огляд"),
     buy: Reviewed("Придбати"),
+    close: Reviewed("Закрити"),
     copied: Reviewed("Скопійовано"),
     copy: Reviewed("Копіювати"),
     copy_link: Reviewed("Копіювати посилання"),
@@ -24,7 +26,7 @@ pub const UK: Translations = Translations {
     cover_image: Reviewed("Зображення обкладинки"),
     default_unlock_info: Reviewed("Щоб дістатися цих завантажень, вам потрібно ввести код. Запитайте в операторів сайту, як його отримати."),
     download: Reviewed("Завантажити"),
-    download_code_seems_incorrect: Unreviewed("Код розблокування виглядає хибним. Перевірте, чи нема одруку."),
+    download_code_seems_incorrect: Reviewed("Код розблокування виглядає хибним. Перевірте, чи нема одруку."),
     downloads: Reviewed("Завантаження"),
     downloads_permalink: Reviewed("downloads"),
     embed: Reviewed("Вбудувати"),
@@ -34,6 +36,7 @@ pub const UK: Translations = Translations {
     extras: Reviewed("Додатки"),
     failed: Reviewed("Помилка"),
     feed: Reviewed("Стрічка"),
+    generic_rss: Reviewed("Звичайна RSS"),
     image_descriptions: Reviewed("Описи зображень"),
     image_descriptions_guide: Reviewed("\
 Мільйони людей використовують веб за допомогою програм \
@@ -52,20 +55,35 @@ pub const UK: Translations = Translations {
 - Не інтерпретуйте. Описуйте, що зображено й що слід знати для розуміння. Не аналізуйте докладніше.<br>\
 - Використовуйте кольори, де в цьому є сенс. Багато хто втратили зір пізно в житті, кольори тішать їхню пам'ять."),
     image_descriptions_permalink: Reviewed("image-descriptions"),
+    javascript_is_disabled_listen_at_xxx: Reviewed("JavaScript вимкнено. Слухати: {link}"),
+    javascript_is_disabled_text: Reviewed("JavaScript вимкнено — деякі можливості недоступні"),
     listen: Reviewed("Прослухати"),
     loading: Reviewed("Завантаження"),
+    m3u_playlist: Reviewed("M3U-добірка"),
     made_or_arranged_payment: Reviewed("Платіж здійснено"),
     missing_image_description_note: Reviewed("Бракує опису картинки<br>Натисніть, щоб дізнатися більше"),
     more: Reviewed("Більше"),
+    mute: Reviewed("Вимкнути звук"),
     name_your_price: Reviewed("Назвіть свою ціну"),
     next_track: Reviewed("Наступна доріжка"),
+    nothing_found_for_xxx: Reviewed("«{query}» не знайдено'"),
     pause: Reviewed("Призупинити"),
     play: Reviewed("Відтворити"),
+    playback_position: Reviewed("Зараз відтворюється"),
+    player_closed: Reviewed("Програвач закрито"),
+    player_open_playing_xxx: Reviewed("Програвач відтворює {title}"),
+    player_open_with_xxx: Reviewed("У програвачі відкрито {title}"),
     previous_track: Reviewed("Попередня доріжка"),
-    price: Unreviewed("Фіксована ціна:"),
+    price: Reviewed("Ціна:"),
     purchase_downloads: Reviewed("Купити завантаження"),
     purchase_permalink: Reviewed("purchase"),
     recommended_format: Reviewed("Рекомендований формат"),
+    search: Reviewed("Пошук"),
+    showing_featured_items: Reviewed("Показано обрані елементи"),
+    showing_xxx_results_for_xxx: Reviewed("Результати ({count}) пошуку «{query}»"),
+    skip_to_main_content: Reviewed("Перейти до головного вмісту"),
+    subscribe: Reviewed("Підписатись"),
+    subscribe_permalink: Reviewed("subscribe"),
     unlisted: Reviewed("Поза списком"),
     unlock: Reviewed("Розблокувати"),
     unlock_downloads: Reviewed("Розблокувати завантаження"),
@@ -80,10 +98,14 @@ pub const UK: Translations = Translations {
 Замініть кінець адреси — /{unlock_permalink}/{page_hash}{index_suffix} — \
 на /{downloads_permalink}/[your-unlock-code]{index_suffix} і тоді натисніть Enter."),
     unlock_permalink: Reviewed("unlock"),
+    unmute: Reviewed("Увімкнути звук"),
     up_to_xxx: Reviewed("До {xxx}"),
     visual_impairment: Reviewed("Вади зору"),
     volume: Reviewed("Гучність"),
     xxx_and_others: Reviewed(r#"{xxx} та <a href="{others_link}">інші</a>"#),
+    xxx_hours: Reviewed("{xxx} год"),
+    xxx_minutes: Reviewed("{xxx} хв"),
     xxx_or_more: Reviewed("{xxx} чи більше"),
+    xxx_seconds: Reviewed("{xxx} с"),
     ..Translations::UNTRANSLATED
 };
