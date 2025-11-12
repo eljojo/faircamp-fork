92700bd07dc105b879a80bc50987062452b5357c
Simon Repp <simon@fdpl.io>
2025-11-12 10:31:45 +0100

    Serialize to more compact js code when generating browser.js

--- src/scripts/browser.rs
+++ src/scripts/browser.rs
@@ -16,15 +16,21 @@ const BROWSER_JS_FILENAME: &str = "browser.js";
 
 fn artist_js_object(artist: &ArtistRc) -> String {
     let artist_ref = artist.borrow();
-    let artist_name_escaped = js_escape_inside_single_quoted_string(&artist_ref.name);
-    let artist_slug = &artist_ref.permalink.slug;
 
-    let mut external_page = String::new();
+    let mut artist_props = Vec::new();
+
     if let Some(url) = &artist_ref.external_page {
-        external_page.push_str(&format!("externalPage:'{url}',"));
+        artist_props.push(format!("externalPage:'{url}'"));
     }
 
-    format!("{{{external_page}name:'{artist_name_escaped}',url:'{artist_slug}/'}}")
+    let artist_name_escaped = js_escape_inside_single_quoted_string(&artist_ref.name);
+    artist_props.push(format!("name:'{artist_name_escaped}'"));
+
+    let artist_slug = &artist_ref.permalink.slug;
+    artist_props.push(format!("url:'{artist_slug}/'"));
+
+    let artist_props_joined = artist_props.join(",");
+    format!("{{{artist_props_joined}}}")
 }
 
 pub fn generate_browser_js(build: &mut Build, catalog: &Catalog) {
@@ -39,112 +45,98 @@ pub fn generate_browser_js(build: &mut Build, catalog: &Catalog) {
             let release_ref = release.borrow();
             let release_slug = &release_ref.permalink.slug;
 
-            let r_tracks = release_ref.tracks
-                .iter()
-                .zip(TRACK_NUMBERS)
-                .map(|(track, track_number)| {
-                    let track_number_formatted = release_ref.track_numbering.format(track_number);
+            let mut release_props = Vec::new();
 
-                    let r_cover = if let Some(src) = track.cover_160_filename() {
-                        format!("cover: '{src}',")
-                    } else {
-                        String::new()
-                    };
-
-                    let track_title_escaped = js_escape_inside_single_quoted_string(&track.title());
-
-                    let r_artists = if catalog.label_mode {
-                        let joined = track.artists
-                            .iter()
-                            .map(artist_js_object)
-                            .collect::<Vec<String>>()
-                            .join(",\n");
-
-                        format!("artists:[{joined}],")
-                    } else {
-                        String::new()
-                    };
-
-                    formatdoc!(r#"
-                        {{
-                            {r_artists}
-                            {r_cover}
-                            number: '{track_number_formatted}',
-                            title: '{track_title_escaped}',
-                            url: '{release_slug}/{track_number}/'
-                        }}
-                    "#)
-                })
-                .collect::<Vec<String>>()
-                .join(",\n");
-
-            let r_artists = if catalog.label_mode {
-                let joined = release_ref.main_artists
+            if catalog.label_mode {
+                let artists_joined = release_ref.main_artists
                     .iter()
                     .map(artist_js_object)
                     .collect::<Vec<String>>()
-                    .join(",\n");
+                    .join(",");
 
-                formatdoc!(r#"
-                    artists: [
-                        {joined}
-                    ],
-                "#)
-            } else {
-                String::new()
-            };
+                release_props.push(format!("artists:[{artists_joined}]"));
+            }
 
-            let r_cover = if let Some(src) = release_ref.cover_160_filename() {
-                format!("cover: '{src}',")
+            if let Some(src) = release_ref.cover_160_filename() {
+                release_props.push(format!("cover:'{src}'"));
             } else {
                 let src = release_ref.procedural_cover_120_filename_unchecked();
-                format!("coverProcedural: '{src}',")
-            };
+                release_props.push(format!("coverProcedural:'{src}'"));
+            }
+
             let release_title_escaped = js_escape_inside_single_quoted_string(&release_ref.title);
+            release_props.push(format!("title:'{release_title_escaped}'"));
 
-            formatdoc!(r#"
-                {{
-                    {r_artists}
-                    {r_cover}
-                    title: '{release_title_escaped}',
-                    tracks: [
-                        {r_tracks}
-                    ],
-                    url: '{release_slug}/'
-                }}
-            "#)
-        })
-        .collect::<Vec<String>>()
-        .join(",\n");
-
-    let r_artists = match catalog.label_mode {
-        true => {
-            catalog.featured_artists
+            let tracks = release_ref.tracks
                 .iter()
-                .filter(|artist| !artist.borrow().unlisted)
-                .map(|artist| {
-                    let artist_ref = artist.borrow();
-                    let artist_name_escaped = js_escape_inside_single_quoted_string(&artist_ref.name);
-                    let artist_slug = &artist_ref.permalink.slug;
+                .zip(TRACK_NUMBERS)
+                .map(|(track, track_number)| {
+                    let mut track_props = Vec::new();
 
-                    let image = if let Some(src) = artist_ref.thumbnail_image_src() {
-                        format!("image: '{src}',")
-                    } else {
-                        String::new()
-                    };
+                    if catalog.label_mode {
+                        let artists_joined = track.artists
+                            .iter()
+                            .map(artist_js_object)
+                            .collect::<Vec<String>>()
+                            .join(",");
 
-                    formatdoc!(r#"
-                        {{
-                            {image}
-                            name: '{artist_name_escaped}',
-                            url: '{artist_slug}/'
-                        }}
-                    "#)
+                        track_props.push(format!("artists:[{artists_joined}]"));
+                    }
+
+                    if let Some(src) = track.cover_160_filename() {
+                        track_props.push(format!("cover:'{src}'"));
+                    }
+
+                    let track_number_formatted = release_ref.track_numbering.format(track_number);
+                    track_props.push(format!("number:'{track_number_formatted}'"));
+
+                    let track_title_escaped = js_escape_inside_single_quoted_string(&track.title());
+                    track_props.push(format!("title:'{track_title_escaped}'"));
+
+                    track_props.push(format!("url:'{release_slug}/{track_number}/'"));
+
+                    let track_props_joined = track_props.join(",");
+                    format!("{{{track_props_joined}}}")
                 })
                 .collect::<Vec<String>>()
-                .join(",\n")
-        }
-        false => String::new()
+                .join(",");
+
+            release_props.push(format!("tracks:[{tracks}]"));
+
+            release_props.push(format!("url:'{release_slug}/'"));
+
+            let release_props_joined = release_props.join(",");
+            format!("{{{release_props_joined}}}")
+        })
+        .collect::<Vec<String>>()
+        .join(",");
+
+    let r_artists = if catalog.label_mode {
+        catalog.featured_artists
+            .iter()
+            .filter(|artist| !artist.borrow().unlisted)
+            .map(|artist| {
+                let artist_ref = artist.borrow();
+
+                let mut artist_props = Vec::new();
+
+                if let Some(src) = artist_ref.thumbnail_image_src() {
+                    artist_props.push(format!("image:'{src}'"));
+                }
+
+                let artist_name_escaped = js_escape_inside_single_quoted_string(&artist_ref.name);
+                artist_props.push(format!("name:'{artist_name_escaped}'"));
+
+                let artist_slug = &artist_ref.permalink.slug;
+                artist_props.push(format!("url:'{artist_slug}/'"));
+
+                let artist_props_joined = artist_props.join(",");
+                format!("{{{artist_props_joined}}}")
+            })
+            .collect::<Vec<String>>()
+            .join(",")
+    } else {
+        String::new()
     };
 
     let label_mode_bool = if catalog.label_mode { "true" } else { "false" };
@@ -155,10 +147,10 @@ pub fn generate_browser_js(build: &mut Build, catalog: &Catalog) {
     let t_xxx_and_others = &build.locale.translations.xxx_and_others;
     let mut js = formatdoc!(r#"
         const BROWSER_JS_T = {{
-            nothingFoundForXxx: query => '{t_nothing_found_for_xxx}'.replace('{{query}}', query),
+            nothingFoundForXxx: query => '{t_nothing_found_for_xxx}'.replace('{{query}}',query),
             showingFeaturedItems: '{t_showing_featured_items}',
-            showingXxxResultsForXxx: (count, query) => '{t_showing_xxx_results_for_xxx}'.replace('{{count}}', count).replace('{{query}}', query),
-            xxxAndOthers: (xxx, othersLink) => '{t_xxx_and_others}'.replace('{{xxx}}', xxx).replace('{{others_link}}', othersLink)
+            showingXxxResultsForXxx: (count,query) => '{t_showing_xxx_results_for_xxx}'.replace('{{count}}',count).replace('{{query}}',query),
+            xxxAndOthers: (xxx,othersLink) => '{t_xxx_and_others}'.replace('{{xxx}}',xxx).replace('{{others_link}}',othersLink)
         }};
         const LABEL_MODE = {label_mode_bool};
         const ARTISTS = [{r_artists}];
